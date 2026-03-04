c0a94120abfee06806cca1dc486c90476504f7b8
praveenamarie <jemipraveenamarie@gmail.com>
2026-03-04 10:47:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# Static Numbers Example
+
+num1 = 10
+num2 = 5
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+division = num1 / num2
+
+print("First number:", num1)
+print("Second number:", num2)
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Division:", division)
